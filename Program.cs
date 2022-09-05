5a27116b3af97fb20cb4eea9bb0ff817031e661c
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-09-05 12:21:30 +0900

    中括弧の付け方の修正とよく使うであろうものをusingに追加

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Program {
-  public static void Main(string[] args) {
+public class Program 
+{
+  public static void Main(string[] args) 
+  {
     Console.WriteLine("Hello World!");
   }
 }
